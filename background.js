92d6be35c91fda91aa3192fe2fae1ebe8fceda2d
123zoeyyy <yzou10@uoguelph.ca>
2020-12-01 14:34:44 +0800

    add: support check trust host local

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 chrome.runtime.onInstalled.addListener(() => {
-
     // Replace all rules
     chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
         // With a new rule
@@ -17,9 +16,28 @@ chrome.runtime.onInstalled.addListener(() => {
             }
         ])
     })
-    chrome.storage.sync.set({color: "#3aa757"}, () => {
-        console.log("The color is green");
-    })
 
+    const url = chrome.runtime.getURL("data/security_urls.json");
+    fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(json => {
+            chrome.storage.local.set(json, () => {
+                console.log("save successful");
+            });
+            chrome.storage.local.set({"fuck": false, "fuck1": true}, () => {
+                console.log("aaaa");
+            });
+        })
+        .catch(err => {
+            chrome.storage.local.set({"fuck": err.message}, () => {
+                console.log(err);
+            })
+        });
+
+    chrome.storage.local.set({color: "#3aa757"}, () => {
+        console.log("The color is green");
+    });
 });
 
--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,103 @@
-console.log("I am content Script")
-const links = document.getElementsByTagName("a");
-for (let i = 0; i < links.length; i++) {
-    console.log(links.item(i));
-    links.item(i).style = "border:1px solid  #999999";
+/**
+ * Try to get some item in localstorage, which these item's key in keys
+ * @param keys
+ * @returns {Promise<{[key: string]:any}>}
+ */
+function getLocalStorageAsync(keys) {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get(keys, result => {
+            for (let i = 0; i < keys.length; i++) {
+                if (!!result[keys[i]])
+                    resolve(result);
+            }
+            reject(new Error("Not found"));
+        });
+    })
 }
+
+/**
+ *
+ * @param host
+ * @returns {Promise<boolean>}
+ */
+function canTrust(host) {
+    let currentHost = host;
+    return getLocalStorageAsync([currentHost])
+        .then(res => {
+            return Promise.resolve(true);
+        })
+        .catch(err => {
+            // complete host can not be trust
+            const names = currentHost.split(".");
+            const hosts = [];
+            for (let i = 0; i < names.length - 1; i++) {
+                currentHost = currentHost.substr(names[i].length + 1, currentHost.length - names[i].length - 1);
+                hosts.push(currentHost);
+            }
+            return getLocalStorageAsync(hosts);
+        })
+        .then(res => {
+            const keys = Object.keys(res);
+            for (let i = 0; i < keys.length; i++) {
+                if (!!res[keys[i]]) {
+                    return Promise.resolve(true);
+                }
+            }
+            return Promise.resolve(false);
+        })
+}
+
+async function asyncFilter(arr, predicate) {
+    const results = await Promise.all(arr.map(predicate));
+    return arr.filter((_v, index) => results[index]);
+}
+
+// First, judge current page host is trust
+canTrust(window.location.host)
+    .then(async canTrustCurrentPageHost => {
+        // Get all a element in this page
+        const aElements = document.getElementsByTagName("a");
+        const requestData = {};
+        let links = [];
+        for (let i = 0; i < aElements.length; i++) {
+            const item = aElements.item(i);
+            if (!!item.host) {
+                if (!requestData[item.host]) {
+                    requestData[item.host] = [];
+                }
+                requestData[item.host].push(item.href);
+            } else {
+                console.log(item.href);
+            }
+        }
+        for (let key in requestData) {
+            links.push({
+                host: key,
+                urls: requestData[key]
+            });
+        }
+
+        // filter not trust host and it's urls
+        links = await asyncFilter(links, async item => {
+            return !await canTrust(item.host);
+        });
+
+        console.log("??", links, "??");
+        // for (let i = 0; i < links.length; i++) {
+        //
+        //     if (!!item.host) {
+        //         links.item(i).style = "border:1px solid  #999999";
+        //         chrome.storage.local.get([item.host], result => {
+        //             // console.log(result);
+        //             if (!!result[item.host]) {
+        //                 item.style = "border:1px solid green";
+        //             }
+        //         });
+        //     }
+        // }
+    })
+    .catch(err => {
+        console.log(err.message);
+    });
+
+
